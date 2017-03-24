940a092d996b2f211e9d507ce0a845366aa22b1f
Admin <vanpho02@gmail.com>
2017-03-24 19:54:26 +0700

    spread operator

--- src/app.js
+++ src/app.js
@@ -5,3 +5,12 @@ ReactDOM.render(
     <p>Helo</p>,
     document.getElementById('root')
 );
+
+// spread operator
+
+const a = { name: 'Pho' };
+let b = { ...a };
+a.name = 'SOn';
+
+console.log(b);
+console.log(a);
