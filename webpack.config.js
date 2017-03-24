1cfb4484d0dbd39fdf38f78450a6f1d86fe832d9
Admin <vanpho02@gmail.com>
2017-03-24 19:34:31 +0700

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname,
+        filename: 'public/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.jsx?$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'cheap-module-source-map'
+};
